760f530225bbba51429e3da85bf36075b6fbd929
Terra Andoh Baffoe <terra.andoh@gmail.com>
2020-07-23 13:52:06 +0000

    Initial Commit: Basic structure setup complete.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="https://img.icons8.com/color/48/000000/task.png"/>
+    <title>Project Management System</title>
+</head>
+<body>
+    <h1>Project Management System</h1>
+</body>
+</html>
