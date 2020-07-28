d83ff844bffc0166075b86975e2275de689a6b29
Terra Andoh Baffoe <terra.andoh@gmail.com>
2020-07-28 16:39:16 +0000

    Feat: Student view project page with buttons and modal created.

--- includes/header.php
+++ includes/header.php
@@ -7,8 +7,10 @@
     <link rel="shortcut icon" href="https://img.icons8.com/color/48/000000/task.png"/>
     <!-- Bootstrap  -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <!-- Font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- Custom css  -->
-    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/index.css?v=<?php echo time(); ?>">
     <noscript>Please enable javascript to use this application</noscript>
     <title>CU PMS</title>
 </head>
---
+++ view.php
@@ -0,0 +1,79 @@
+<?php 
+include "./includes/header.php";
+if (empty($_SESSION['name'])) {
+    header("Location: index.php");
+}
+?>
+<div class="dashboard">
+    <?php include "./includes/navbar.php" ?>
+
+    <div class="dashboard-view">
+        <div class="jumbotron border-radius-0">
+            <div class="container">
+                <h1 class="display-4">Project Title</h1>
+                <p class="lead">By: Student name</p>
+                <hr class="my-4">
+                <div class="p-5 bg-secondary align-items-center row">
+                    <div class="col-md-4 text-center">
+                        <div class="card m-auto text-center rounded" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title text-dark"><i class="fa fa-download"></i></h5>
+                            <p class="card-text">Uploaded project document</p>
+                            <a href="#" class="btn btn-dark">Download</a>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 text-center">
+                        <div class="card m-auto text-center rounded" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title text-dark"><i class="fa fa-pencil"></i></h5>
+                            <p class="card-text">View and edit document</p>
+                            <a href="editDocument.php" class="btn btn-dark">View</a>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 text-center">
+                        <div class="card m-auto text-center rounded" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title text-danger"><i class="fa fa-trash"></i></h5>
+                            <p class="card-text text-danger">Put document in trash</p>
+                            <a href="#" class="btn btn-danger" data-toggle="modal" data-target="#confirmModal">Delete</a>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    <div>
+        
+</div>
+
+<!-- Confirm modal  -->
+<div class="modal fade" id="confirmModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content text-center">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Confirmation</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <h4 class="text-danger">Are you sure you want to delete your project document ?</h4>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-dark" data-dismiss="modal">No</button>
+        <button type="button" class="btn btn-secondary">Delete</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
+<div class="bg-secondary p-3 bg-secondary footer align-content-center">
+    <p class="text-center text-light m-auto p-1">CU PMS &copy; 2020</p>
+</div>
+
+
+<?php include "./includes/footer.php" ?>
