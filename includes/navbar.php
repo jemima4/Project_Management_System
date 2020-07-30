51650e1a8d6de5e9641b7c0a8a2b940a69ffadaa
Terra Andoh Baffoe <terra.andoh@gmail.com>
2020-07-30 13:56:26 +0000

    Feat: Navbar options for lecturer updated.

--- includes/navbar.php
+++ includes/navbar.php
@@ -13,8 +13,10 @@
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <div class="navbar-nav">
                 <a class="nav-item nav-link active" href="dashboard.php">Home <span class="sr-only">(current)</span></a>
-                <?php if(!isset($_SESSION["projectid"])): ?>
+                <?php if(!isset($_SESSION["projectid"]) AND ($_SESSION['currentUser']) === "student"): ?>
                     <a class="nav-item nav-link" href="./create.php">Create Project</a>
+                <?php elseif($_SESSION['currentUser'] === "lecturer"): ?>
+                    <a class="nav-item nav-link" href="./view.php">View Students</a>
                 <?php else: ?>
                     <a class="nav-item nav-link" href="./view.php">View Project</a>
                 <?php endif; ?>
